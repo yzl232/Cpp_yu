0e38eda73b319ab08ac1372f08c91a32a0efa11e
ZhenglinYu <buptyuzhenglin@gmail.com>
2014-04-26 12:31:18 -0400

    add sample.

--- 4_attributes_From_eight/original_main.cpp
+++ 4_attributes_From_eight/original_main.cpp
@@ -11,7 +11,7 @@
 #include <gzstream.h>
 #include "ams.h"
 #include "prng.h"
-#include <ctime>
+
 using namespace std;
 
 /*** Functions for converting int to string ***/
@@ -82,7 +82,7 @@ int main(int argc, char** argv) {
 
 		ofstream pFile;
 		pFile.open(
-				"/export/qhome/zyu/intersect/zhenglin/data/src/ratio_five_percent_results_32768.txt.txt",
+				"/export/qhome/zyu/intersect/zhenglin/data/src/redo_experiments_1024.txt",
 				ios::app);
 
 		if (pFile != NULL) {
@@ -97,7 +97,7 @@ int main(int argc, char** argv) {
 
 			pFile.close();
 			pFile.open(
-					"/export/qhome/zyu/intersect/zhenglin/data/src/ratio_five_percent_results_32768.txt.txt",
+					"/export/qhome/zyu/intersect/zhenglin/data/src/redo_experiments_1024.txt",
 					ios::app);
 
 		}
@@ -111,18 +111,13 @@ int main(int argc, char** argv) {
 				"UK/BTCUK", "UK/BWYUK", "UK/CNCUK", "UK/STCUK", "UK/VNNUK",
 				"UK/VTCUK", "WS/AZNMN", "WS/COUTW", "WS/LASCA", "WS/SDLVH",
 				"WS/SFNSA", "WS/SORID" };
-		int num_regions = 1; //36;
+		int num_regions = 36; //36;
 		prng_type *prng;
 		prng = prng_Init(-time(NULL), 2);
 
 		int day = 1, month = 1, year = 2014;
-
-		int buckets = 32768, depth = 1, s1 = 1;
-
-	    string weekdays[7] = { "Sun", "Mon", "Tues", "Wednes", "Thurs", "Fri",
-	    			"Satur" };
-	    string weekDay;
-	    int weekDayNum;
+		pFile << " line119"<< endl;
+		int buckets = 65536, depth = 1, s1 = 1;
 
 		if (argc > 1)
 			buckets = atoi(argv[1]);
@@ -130,31 +125,31 @@ int main(int argc, char** argv) {
 			s1 = atoi(argv[2]);
 		if (argc > 3)
 			depth = atoi(argv[3]);
-
+		pFile << " line128"<< endl;
 		pFile << "buckets = " << buckets << ", s1 = " << s1 << ", depth = "
 				<< depth << endl;
 
-		int numPairs = 47;
+		int numPairs = 10;
 
 		vector<AMS_type *> sketch1F0(numPairs);
 		vector<AMS_type *> sketch2F0(numPairs);
 		vector<AMS_type *> sketch1F1(numPairs);
 		vector<AMS_type *> sketch2F1(numPairs);
-
+		pFile << " line138"<< endl;
 		double rel_err_F0 = 0.0, rel_err_F1 = 0.0, rel_err_Avg = 0.0;
 		for (int reg = 0; reg < num_regions; ++reg) {
 			// read in the pairs that we wish to sample
 			// RNC(1)/service(17) pairs and item_ID(10)/domain pairs(18)
 			vector<string> pair1, pair2;
-			string
-					sample_filename =
-							"/export/qhome/zyu/intersect/zhenglin/data/src/intersection_ratio_5_percent.txt";
+			string sample_filename = intToString(reg + 1) + "-"
+					+ intToStringPad(year, 4) + "-" + intToStringPad(month, 2)
+					+ "-" + intToStringPad(day, 2) + "-sample.txt";
 			string line;
 			ifstream sample_file(sample_filename.c_str());
 			while (getline(sample_file, line)) {
 				vector < string > tokens = split(line.c_str(), '|');
-				pair1.push_back(tokens[0] + "|" + tokens[2]); // RNC and service
-				pair2.push_back(tokens[1] + "|" + tokens[3]); // item_ID and domain
+				pair1.push_back(tokens[1] + "|" + tokens[17]); // RNC and service
+				pair2.push_back(tokens[10] + "|" + tokens[18]); // item_ID and domain
 			}
 			sample_file.close();
 
@@ -167,7 +162,7 @@ int main(int argc, char** argv) {
 				sketch1F1[i] = AMS_Init(buckets, depth, s1);
 				sketch2F1[i] = AMS_Init(buckets, depth, s1);
 			}
-
+			pFile << " line165"<< endl;
 			vector<long long> count1(numPairs, 0);
 			vector<long long> count2(numPairs, 0);
 			vector<long long> countBoth(numPairs, 0);
@@ -175,87 +170,74 @@ int main(int argc, char** argv) {
 			string region = regions[reg];
 			string super_region = region.substr(0, 2);
 			string sub_region = region.substr(3);
-			for (day = 1; day < 8; ++day) {
-				for (int hr = 0; hr < 24; ++hr) {
-					string hour = intToStringPad(hr, 2);
-					struct tm epoch;
-					epoch.tm_sec = 0;
-					epoch.tm_min = 0;
-					epoch.tm_hour = hr;
-					epoch.tm_mday = day;
-					epoch.tm_mon = month - 1;
-					epoch.tm_year = year - 1900;
-					string epoch_time = intToString(mktime(&epoch));
-					
-					
-				    std::tm time_in = { 0, 0, 0, // second, minute, hour
-				         day-0, month-1, year - 1900 }; // 1-based day, 0-based month, year since 1900
 
-				 std::time_t time_temp = std::mktime( & time_in );
+			for (int hr = 0; hr < 24; ++hr) {
+				string hour = intToStringPad(hr, 2);
+				struct tm epoch;
+				epoch.tm_sec = 0;
+				epoch.tm_min = 0;
+				epoch.tm_hour = hr;
+				epoch.tm_mday = day;
+				epoch.tm_mon = month - 1;
+				epoch.tm_year = year - 1900;
+				string epoch_time = intToString(mktime(&epoch));
 
-				 // the return value from localtime is a static global - do not call
-				 // this function from more than one thread!
-				 std::tm const *time_out = std::localtime( & time_temp );
+				pFile << region << " " << hour << " " << epoch_time << endl;
 
-				 //std::cout << "I was born on (Sunday = 0) D.O.W. " << time_out->tm_wday << '\n';
-				    weekDayNum = time_out->tm_wday;
-				    weekDay =  weekdays[weekDayNum] ;
-					//pFile << region << " " << hour << " " << epoch_time << endl;
+				string filename = base_path + "/" + region + "/FULL/"
+						+ intToStringPad(year, 4) + "/" + intToStringPad(month,
+						2) + "/" + intToStringPad(day, 2) + "/RNC."
+						+ super_region + "." + sub_region + ".PA." + epoch_time
+						+ ".dat.gz";
+				pFile << " line192"<< endl;
+				igzstream in(filename.c_str());
+				string line;
+				while (getline(in, line)) {
+					vector < string > tokens = split(line.c_str(), '|');
+					long long count = atoi(tokens[21].c_str());
+					long long value = atoi(tokens[22].c_str());
+					if (value < 0)
+						value = -value; // prevents overflow but still gives a reasonable update
+					long double v_F0 = F1_precompute(count, prng); // we need to total these values, so we use F1
+					long double v_F1 = F1_precompute(value, prng);
 
-					string filename = base_path + "/" + region + "/FULL/"
-							+ intToStringPad(year, 4) + "/" + intToStringPad(
-							month, 2) + "/" + intToStringPad(day, 2) + "/RNC."
-							+ super_region + "." + sub_region + ".PA."
-							+ epoch_time + ".dat.gz";
+					//pFile << count << " " << v_F0 << ", " << value << "  " << v_F1 << endl;
+					//if (value < 0) { pFile << tokens[21].c_str() << "|" << tokens[22].c_str() << endl; exit(1); }
+					pFile << " line206"<< endl;
+					for (int i = 0; i < numPairs; ++i) {
+						bool inFirst = (tokens[1] + "|" + tokens[17]
+								== pair1[i]);
+						bool inSecond = (tokens[10] + "|" + tokens[18]
+								== pair2[i]);
+						long long flowID = hash_S_LL(line); // NOTE: we hash the entire descriptor (not just the flow ID)
 
-					igzstream in(filename.c_str());
-					string line;
-					while (getline(in, line)) {
-						vector < string > tokens = split(line.c_str(), '|');
-						long long count = atoi(tokens[21].c_str());
-						long long value = atoi(tokens[22].c_str());
-						if (value < 0)
-							value = -value; // prevents overflow but still gives a reasonable update
-						long double v_F0 = F1_precompute(count, prng); // we need to total these values, so we use F1
-						long double v_F1 = F1_precompute(value, prng);
+						pFile << " line214"<< endl;
+						// code for keeping track of true intersection values
+						if (inFirst && inSecond) {
+							realF0[i] += count;
+							realF1[i] += value;
+							countBoth[i] += 1;
+						}
 
-						//pFile << count << " " << v_F0 << ", " << value << "  " << v_F1 << endl;
-						//if (value < 0) { pFile << tokens[21].c_str() << "|" << tokens[22].c_str() << endl; exit(1); }
+						// code for inserting into first sketch
+						if (inFirst) {
+							// insert into first sketch
+							AMS_Update(sketch1F0[i], flowID, v_F0);
+							AMS_Update(sketch1F1[i], flowID, v_F1);
+							count1[i] += 1;
+						}
 
-						for (int i = 0; i < numPairs; ++i) {
-							bool inFirst = (tokens[1] + "|" + tokens[17]  + “|” +   tokens[13]  + “|” + weekDay
-									== pair1[i]);
-							bool inSecond = (tokens[10] + "|" + tokens[18]   + “|” +   tokens[15]  + “|” + hour
-									== pair2[i]);
-							long long flowID = hash_S_LL(line); // NOTE: we hash the entire descriptor (not just the flow ID)
-
-
-							// code for keeping track of true intersection values
-							if (inFirst && inSecond) {
-								realF0[i] += count;
-								realF1[i] += value;
-								countBoth[i] += 1;
-							}
-
-							// code for inserting into first sketch
-							if (inFirst) {
-								// insert into first sketch
-								AMS_Update(sketch1F0[i], flowID, v_F0);
-								AMS_Update(sketch1F1[i], flowID, v_F1);
-								count1[i] += 1;
-							}
-
-							// code for inserting into second sketch
-							if (inSecond) {
-								// insert into second sketch
-								AMS_Update(sketch2F0[i], flowID, v_F0);
-								AMS_Update(sketch2F1[i], flowID, v_F1);
-								count2[i] += 1;
-							}
+						// code for inserting into second sketch
+						if (inSecond) {
+							// insert into second sketch
+							AMS_Update(sketch2F0[i], flowID, v_F0);
+							AMS_Update(sketch2F1[i], flowID, v_F1);
+							count2[i] += 1;
 						}
 					}
 				}
 			}
+
 			long double sumRelErrorF0 = 0.0, sumRelErrorF1 = 0.0,
 					sumRelErrorAvg = 0.0;
 			for (int i = 0; i < numPairs; ++i) {
@@ -315,8 +297,8 @@ int main(int argc, char** argv) {
 				pFile << "intersection ratio: " << ir << "\t" << "errors F0: "
 						<< fabs(realF0[i] - estF0) / realF0[i] << "\t"
 						<< "errors F1 " << fabs(realF1[i] - estF1) / realF1[i]
-						<< "\t" << "errors F1/F0 " << fabs(realAvg - estF1
-						/ estF0) / realAvg << endl;
+						<< "\t" << "errors F2 "
+						<< fabs(realAvg - estF1 / estF0) / realAvg << endl;
 
 				// pFile << ir << "\t" << fabs(realF0[i] - estF0)/realF0[i] << "\t" << fabs(realF1[i] - estF1)/realF1[i] << "\t" << fabs(realAvg - estF1/estF0)/realAvg << endl;
 				/* end code for plotting error vs intersection rate */
@@ -327,7 +309,7 @@ int main(int argc, char** argv) {
 
 				pFile.close();
 				pFile.open(
-						"/export/qhome/zyu/intersect/zhenglin/data/src/ratio_five_percent_results_32768.txt.txt",
+						"/export/qhome/zyu/intersect/zhenglin/data/src/redo_experiments_1024.txt",
 						ios::app);
 				AMS_Destroy(sketchSumF0);
 				AMS_Destroy(sketchSumF1);
